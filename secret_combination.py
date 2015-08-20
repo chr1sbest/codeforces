39d83144751765b0bb313141f1b0c0f13bd45988
Chris Best <chr2sbest@gmail.com>
2015-08-20 10:40:44 -0700

    Added test to secret combination

--- secret_combination.py
+++ secret_combination.py
@@ -1,28 +1,24 @@
 """
+You got a box with a combination lock. The lock has a display showing n digits.
+There are two buttons on the box, each button changes digits on the display.
+You have quickly discovered that the first button adds 1 to all the digits
+(all digits 9 become digits 0), and the second button shifts all the digits on
+the display one position to the right (the last digit becomes the first one). 
+For example, if the display is currently showing number 579, then if we push 
+the first button, the display will show 680, and if after that we push the 
+second button, the display will show 068.
+
+You know that the lock will open if the display is showing the smallest
+possible number that can be obtained by pushing the buttons in some order.
+The leading zeros are ignored while comparing numbers. Now your task is to
+find the desired number.
+
 1) Split the combination into individual integers
 2) Build an array for each possible initial combination (swaps)
 3) Determine amount needed to make array[0] == 0
 4) Add the above amount to entire array
 5) %10 each value to determine true value
 6) Iterate through each combination to determine solution
-
-579 -> 024
-2014 -> 0142
-
-4582 ->  0148
-5824 ->  0379
-8245 ->  0467
-2458 ->  0236
-
-
-initial = 1238
-str(initial) -> '1238'
-['1', '2', '3', '8']
-
-additive = 9
-list = [1,2,3,4]
-
-[0, 0, 2, 3] -> 123
 """
 from collections import deque
 
@@ -52,3 +48,8 @@ def secret_combination(length, initial):
     secret_combination = ['0'] * length  # Initialize 0's
     secret_combination[-len(min_array):] = min_array
     return ''.join(secret_combination)
+
+def test_secret_combination():
+    assert '024' == secret_combination(3, 579)
+    assert '0142' == secret_combination(4, 2014)
+    assert '0148' == secret_combination(4, 4582)
